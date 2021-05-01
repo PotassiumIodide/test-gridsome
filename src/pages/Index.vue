38d4e9f58662f22a6bb003a040cba2a2c08364d4
PotassiumIodide <potassium.iodide28@gmail.com>
2021-05-01 13:13:45 +0900

    [Change] Move header and footer into Layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,23 +1,6 @@
 <template>
   <Layout>
     <div>
-      <header class="header">
-	      <div class="container">
-		      <div class="site">
-			      <a href="base-index.html">
-				      <img src="images/logo.svg" alt="ESSENTIALS">
-			      </a>
-		      </div>
-		
-		      <nav class="nav">
-			      <ul>
-				      <li><a href="base-index.html">TOP</a></li>
-				      <li><a href="base-about.html">ABOUT</a></li>
-			      </ul>
-		      </nav>
-	      </div>
-      </header>
-
       <section class="hero">
 	      <figure>
 		      <img src="images/hero.jpg" alt="">
@@ -73,38 +56,6 @@
 		      <img src="images/berry.jpg" alt="赤く熟したベリー">
 	      </figure>
       </section>
-
-      <footer class="footer">
-	      <div class="container">
-		      <div class="site">
-		      <a href="base-index.html">
-			      <img src="images/logo-w.svg" alt="ESSENTIALS">
-			      <p>おいしい食材と食事を探求するサイト</p>
-		      </a>
-		      </div>
-
-		      <ul class="sns">
-			      <li>
-				      <a href="https://twitter.com/">
-					      <i class="fab fa-twitter"></i>
-					      <span class="sr-only">Twitter</span>
-				      </a>
-			      </li>
-			      <li>
-				      <a href="https://facebook.com/">
-					      <i class="fab fa-facebook-square"></i>
-					      <span class="sr-only">Facebook</span>
-				      </a>
-			      </li>
-			      <li>
-				      <a href="http://instagram.com/">
-					      <i class="fab fa-instagram"></i>
-					      <span class="sr-only">Instagram</span>
-				      </a>
-			      </li>
-		      </ul>
-	      </div>
-      </footer>
     </div>
   </Layout>
 </template>
